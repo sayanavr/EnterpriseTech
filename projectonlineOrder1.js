2b47c06a3b711a7ef958f74f9ec1833a5fce6057
TulsiPatel11 <69999590+TulsiPatel11@users.noreply.github.com>
2020-08-20 22:49:29 -0400

    Create projectonlineOrder1.js

---
+++ projectonlineOrder1.js
@@ -0,0 +1,24 @@
+# EnterpriseTech
+var mongoclient= require('mongodb').MongoClient;
+const uri ="mongodb+srv://Sayana:Enterprise123@cluster0.mtc90.mongodb.net/CoffeeShop?retryWrites=true&w=majority";
+const client = new mongoclient(uri, { useNewUrlParser: true });
+var myObj=[{oo_id:"o_001",item_id:"M001",price:1.5}];
+client.connect((err, db) => {
+    if (err) {
+        console.log("cannot connect db" + err);
+        return;
+    }
+    console.log("DataBase connection made successfully");
+    const collection = db.db().collection("onlineOrder");
+
+    collection.insertMany(myObj, function (err, r) {
+        if (err) {
+            console.log("cannot add obj");
+            return;
+        }
+
+        console.log("Inserted rows!");
+    });
+    client.close();
+});
+
